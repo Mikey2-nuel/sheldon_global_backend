4e0ef6f32735ac54c0752c41b507d0018b499c8f
Nwoye Emmanuel Emeka <emmanuelnwoye12@gmail.com>
2025-09-04 21:56:33 +0100

    Initial commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
 }).promise();
 
 db.connect((err) => {
